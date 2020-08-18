8f1712386a3b84e9cb96559a71d889c3d6eb7578
Matheus Reis <matheus.reis@olist.com>
2020-08-18 16:11:52 -0300

    init method in base dao

--- app/dao/base_dao.py
+++ app/dao/base_dao.py
@@ -3,15 +3,21 @@ from sqlalchemy.orm.session import sessionmaker
 
 class BaseDao:
 
-    def __init__(self):
-        pass
+    def __init__(self, model_class):
+        self.__connector = ''
+        self.__hostname = ''
+        self.__username = ''
+        self.__password = ''
+        self.__database = ''
+        self.__get_connection()
+        self.__model_class = model_class
 
     def __get_connection(self):
         pass
 
     def read(self, id: str = ''):
         pass
-    
+
     def insert(self, model):
         pass
 
