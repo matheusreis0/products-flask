9ffecb440405c5801f663912319caa12083d6fc9
Matheus Reis <matheus.reis@olist.com>
2020-08-18 16:15:45 -0300

    method get connection

--- app/dao/base_dao.py
+++ app/dao/base_dao.py
@@ -1,6 +1,7 @@
 import sqlalchemy as db
 from sqlalchemy.orm.session import sessionmaker
 
+
 class BaseDao:
 
     def __init__(self, model_class):
@@ -13,7 +14,12 @@ class BaseDao:
         self.__model_class = model_class
 
     def __get_connection(self):
-        pass
+        engine = db.create_engine(
+            f'{self.__connector}://{self.__username}:{self.__password}@{self.__hostname}/{self.__database}'
+        )
+        Session = db.orm.sessionmaker()
+        Session.configure(bind=engine)
+        self.__session = Session()
 
     def read(self, id: str = ''):
         pass
